6d420b330c8f87deebb3223becfb7eb1aa97a6f4
Alisson Andrade <aqandrade23@gmail.com>
2022-02-15 18:14:46 -0300

    refactor: type return of method convertAmount

--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ConvertAmountDto } from './dto/convert-amount.dto';
 import { ExchangeService } from './exchange.service';
+import { ExchangeType } from './types/exchange.type';
 
 @Controller('exchange')
 export class ExchangeController {
   constructor(private exchangeService: ExchangeService) {}
 
   @Get()
-  async convertAmount(convertAMountDto: ConvertAmountDto) {
+  async convertAmount(
+    @Query() convertAMountDto: ConvertAmountDto,
+  ): Promise<ExchangeType> {
     return await this.exchangeService.convertAmount(convertAMountDto);
   }
 }
